bdbd07ad15a978fe3104d8664b2c38ee9c449fee
Spencer Lynch <114882706+SpenceDog@users.noreply.github.com>
2023-05-25 00:06:27 -0400

    Video 27
    Watched up to video #27.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+require 'Response.php';
+require 'router.php';
+
+// connect to our MySQL database.
+/*
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+dd($posts);
+*/
