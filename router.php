e6bb02d941dccd5b36abaee12e5cc21b600a59cf
Spencer Lynch <114882706+SpenceDog@users.noreply.github.com>
2023-06-13 19:15:10 -0400

    Chapter 30
    Chapter 30 is completed!
    Please note, the document root needs to be changed to "public."
    
    This command works for Laragon:
    php -S localhost:80 -t public

--- router.php
+++ router.php
@@ -1,22 +1,22 @@
 <?php
 
-$routes = require('routes.php');
-
-function routeToController($uri, $routes) {
-if (array_key_exists($uri, $routes)) {
-require $routes[$uri];
-} else {
-abort();
-}
+ function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
 }
 
 function abort($code = 404) {
-http_response_code(404);
+    http_response_code($code);
 
-require "views/{$code}.php";
+    require "views/{$code}.php";
 
-die();
+    die();
 }
 
+$routes = require('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
 routeToController($uri, $routes);
